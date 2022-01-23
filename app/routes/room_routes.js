29510a27fa852f2ffd0deadad13f1eeb5e222dff
hanif <h.ehmedov@gmail.com>
2022-01-23 02:56:19 -0500

    socket io test setup done

--- app/routes/room_routes.js
+++ app/routes/room_routes.js
@@ -4,34 +4,38 @@ const passport = require('passport')
 const router = express.Router()
 const customErrors = require('../../lib/custom_errors')
 const Room = require('../models/rooms.js')
+const asyncHandler = require('express-async-handler')
 
-// we'll use this function to send 404 when non-existant document is requested
 const requireToken = passport.authenticate('bearer', { session: false })
-const handle404 = customErrors.handle404
-// we'll use this function to send 401 when a user tries to modify a resource
-// that's owned by someone else
-const requireOwnership = customErrors.requireOwnership
+const { BadCredentialsError } = customErrors
+/* Errors: BadParamsError, handle404, requireOwnership */
 
-router.post('/create-room', requireToken, (req, res, next) => {
-  req.body.room.owner = req.user._id
-  console.log(req.body)
+router.post(
+  '/create-room',
+  requireToken,
+  asyncHandler(async (req, res, next) => {
+    req.body.room.owner = req.user._id
 
-  Room.create(req.body.room)
-    .then(room => {
-      res.status(201).json({ room })
-    })
-    .catch(next)
-})
+    if (!req.body.room.name) {
+      throw new BadCredentialsError()
+    }
 
-router.get('/Show-rooms/:id', requireToken, (req, res, next) => {
-  req.body.room.owner = req.user._id
-  console.log(req.body)
+    const room = await Room.create(req.body.room)
 
-  Room.create(req.body.room)
-    .then((room) => {
-      res.status(201).json({ room })
-    })
-    .catch(next)
-})
+    res.status(201).json({ room })
+  })
+)
+
+router.get(
+  '/index-rooms',
+  requireToken,
+  asyncHandler(async (req, res, next) => {
+    console.log('got')
+    // get all rooms
+    const rooms = await Room.find()
+
+    res.status(200).json({ rooms })
+  })
+)
 
 module.exports = router
--- src/SocketListeners.js
+++ src/SocketListeners.js
@@ -1,20 +1,87 @@
-const IoServer = require('./IoServer')
+/* const Room = require('../app/models/rooms')
 // const UserSocket = require('./UserSocket')
 
-console.log(IoServer)
-
 const connected = []
+const rooms = []
+// {roomId: asdasd, users: []}
 
-IoServer.events.on('new-connection', (socket) => {
-  console.log('user joined')
+const newConnection = (socket) => {
   console.log('socket connected: ')
   connected.push(socket) // TODO: going to be a DB object
-})
+}
 
-IoServer.events.on('join', async (req, socket) => {
-  await socket.join(req.roomId.toString())
+const destroySocket = (socketId) => {
+  const index = connected.findIndex((socket) => socket.id === socketId)
+  if (index !== -1) {
+    connected.splice(index, 1)
+  }
+}
 
-  // for testing
-  IoServer.send('test', req.roomId.toString())
-  IoServer.send('test', 'jonahs-room')
-})
+const addListeners = (server) => {
+  console.log('calling addlisteners')
+  server.events.on('new-connection', newConnection)
+}
+
+const joinRoom = (roomId, socket) => {
+  const existingRoom = rooms.find((room) => room.id === roomId)
+  if (existingRoom) {
+    existingRoom.users.push(socket)
+    return
+  }
+  rooms.push({ roomId, users: [socket] })
+}
+
+const deleteRoom = (roomId, server, userId, cb) => {
+  // console.log(server.server.sockets)
+  console.log('ATTEMPTING TO DELETE ROOMID=')
+  console.log(userId)
+  const connected = server.server.sockets.adapter.rooms.get(roomId)
+  console.log('BEFORE LEAVING')
+  console.log(connected)
+  Room.deleteOne({ _id: roomId, owner: userId }).then((obj) => {
+    console.log('received object')
+    console.log(obj)
+    if (obj.deletedCount > 0) {
+      const room = rooms.find((room) => room.roomId === roomId)
+      if (room) {
+        room.users.forEach((userSocket) => {
+          userSocket.socket.leave(roomId)
+        })
+        console.log('AFTER LEAVING')
+        console.log(server.server.sockets.adapter.rooms.get(roomId))
+        console.log('returning true')
+        return cb(true)
+      }
+      // const connected = server.server.sockets.adapter.rooms.get(roomId)
+      // console.log('BEFORE LEAVING')
+      // console.log(connected)
+      // connected.forEach(client => {
+      //   console.log(client)
+      //   client.leave(roomId)
+
+      // })
+      // server.server.clients(roomId).forEach(socket => socket.leave(roomId))
+    }
+    return cb(false)
+  })
+}
+
+const checkRoomAccess = (userID, roomId) => {
+  Room.findOne({ _id: roomId }, (err, room) => {
+    if (err) return false
+    if (room.validUsers.includes(userID)) {
+      return true
+    } else {
+      return false
+    }
+  })
+}
+
+module.exports = {
+  addListeners,
+  joinRoom,
+  destroySocket,
+  checkRoomAccess,
+  deleteRoom
+}
+*/
